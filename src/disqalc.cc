cad6829e62398255dbc2dfd4c91e7346cd1fc81d
Noa Aarts <noa@voorwaarts.nl>
2025-03-14 11:07:31 +0100

    use printoptions

--- src/disqalc.cc
+++ src/disqalc.cc
@@ -5,15 +5,16 @@ void init_calculator() {
   CALCULATOR->loadExchangeRates();
   CALCULATOR->loadGlobalDefinitions();
   CALCULATOR->loadLocalDefinitions();
+}
+
+rust::String do_calculation(rust::String str) {
   PrintOptions po = PrintOptions();
   po.indicate_infinite_series = true;
   po.allow_non_usable = true;
   po.use_unicode_signs = UNICODE_SIGNS_ON;
   po.decimalpoint_sign = std::string(".");
-  CALCULATOR->setMessagePrintOptions(po);
-}
-
-rust::String do_calculation(rust::String str) {
-  std::string result = CALCULATOR->calculateAndPrint(std::string(str), 2000);
+  std::string result = CALCULATOR->calculateAndPrint(
+      std::string(str), 2000, default_user_evaluation_options, po,
+      AUTOMATIC_FRACTION_AUTO, AUTOMATIC_APPROXIMATION_OFF);
   return rust::String(result);
 }
